e36c8767304bdf4ee692da811a5bb648f45a094a
diegosanchezmartin <sanchhez08@gmail.com>
2022-03-07 16:10:41 +0100

    add new icons

--- trekkingCastillaYLeon/src/views/TabsPage.vue
+++ trekkingCastillaYLeon/src/views/TabsPage.vue
@@ -4,17 +4,17 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
+          <ion-icon :icon="homeOutline" />
           <ion-label>Inicio</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
+          <ion-icon :icon="searchOutline" />
           <ion-label>Descubrir</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="personOutline" />
           <ion-label>Perfil</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -25,16 +25,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { homeOutline, searchOutline, personOutline } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      homeOutline,
+      searchOutline,
+      personOutline,
     }
   }
 });
