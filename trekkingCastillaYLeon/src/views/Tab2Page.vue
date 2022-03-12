9b284451bb2bd93a5beaf546f3dfd89bf0c701d8
diegosanchezmartin <sanchhez08@gmail.com>
2022-03-12 11:19:01 +0100

    Add Adobe XD prototype and fix some bugs

--- trekkingCastillaYLeon/src/views/Tab2Page.vue
+++ trekkingCastillaYLeon/src/views/Tab2Page.vue
@@ -11,8 +11,9 @@
           <ion-title size="large">Descubrir</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <div class="map" ref="mapDivRef">
+
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -20,10 +21,47 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { ref, onBeforeMount } from "vue";
 
 export default defineComponent({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    /*const map = ref(null);
+
+    const mapDivRef = ref(null);
+
+    onBeforeMount(() => {
+      const key = process.env.VUE_APP_GOOGLEMAPS_KEY;
+
+      const googleMapScript = document.createElement("SCRIPT");
+      googleMapScript.setAttribute(
+        "src",
+        'https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap'
+      );
+
+      googleMapScript.setAttribute("defer", "");
+      googleMapScript.setAttribute("async", "");
+      document.head.appendChild(googleMapScript);
+    })
+
+    window.initMap = () => {
+      map.value = new google.maps.Map(mapDivRef.value, {
+        mapTypeId : 'hybrid'
+      });
+    }
+
+    return{
+      mapDivRef
+    };*/
+  }
 });
 </script>
+
+<style scoped>
+.map {
+  width: 100%;
+  height: 300px;
+  background-color: azure;
+}
+</style>
--- trekkingCastillaYLeon/src/views/Tab3Page.vue
+++ trekkingCastillaYLeon/src/views/Tab3Page.vue
@@ -11,8 +11,35 @@
           <ion-title size="large">Usersame</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 3 page" />
+      <ion-card>
+            <ion-card-header>
+                <ion-card-title>
+                    Rutero extremo
+                </ion-card-title>
+                <ion-card-subtitle>
+                    13 rutas publicadas
+                </ion-card-subtitle>
+            </ion-card-header>
+            <ion-card-content>
+              <ion-list>
+                    <ion-item>
+                        <ion-label position="floating">Ruta 1</ion-label>
+                        <ion-img src="https://siempredepaso.es/wp-content/uploads/2016/06/QA2282-1.jpg">
+                        </ion-img>
+                    </ion-item>
+                    <ion-item>
+                        <ion-label position="floating">Ruta 2</ion-label>
+                        <ion-img src="https://img.remediosdigitales.com/b9b399/castillo-de-ponferrada-2/450_1000.jpg">
+                        </ion-img>
+                    </ion-item>
+                    <ion-item>
+                        <ion-label position="floating">Ruta 3</ion-label>
+                        <ion-img src="https://www.sientecastillayleon.com/wp-content/uploads/2015/11/rutas-culturales-en-castilla-y-leon.jpg"> 
+                        </ion-img>
+                    </ion-item>
+              </ion-list>
+            </ion-card-content>
+        </ion-card>
     </ion-content>
   </ion-page>
 </template>
@@ -20,10 +47,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'Tab3Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 });
 </script>
