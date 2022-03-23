6da48d52dc09f0a1cc2f8471e90eada0d708060f
diegosanchezmartin <sanchhez08@gmail.com>
2022-03-23 19:56:42 +0100

    Add new interface to create new routes

---
+++ trekkingCastillaYLeon/src/components/FormularioRuta.vue
@@ -0,0 +1,97 @@
+<template>
+  <form>
+    <ion-list>
+      <ion-item>
+        <ion-label position="floating"
+          >Nombre de la ruta que vas a añadir</ion-label
+        >
+        <ion-input clear-input value="" type="text"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Categoría</ion-label>
+        <ion-select
+          interface="popover"
+          placeholder="Selecciona la categoría de la ruta"
+        >
+          <ion-select-option value="rutaLineal">Ruta líneal </ion-select-option>
+          <ion-select-option value="rutaCircular"
+            >Ruta Circular
+          </ion-select-option>
+          <ion-select-option value="ascension">Ascensión </ion-select-option>
+        </ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Dificultad</ion-label>
+        <ion-select
+          interface="popover"
+          placeholder="Selecciona la dificultad de la ruta"
+        >
+          <ion-select-option value="facil">Fácil</ion-select-option>
+          <ion-select-option value="media">Media</ion-select-option>
+          <ion-select-option value="dificil">Difícil</ion-select-option>
+          <ion-select-option value="extrema">Extrema</ion-select-option>
+        </ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating"
+          >Introduce cuántos kilómetros has recorrido</ion-label
+        >
+        <ion-input clear-input value="" type="text"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Valoración</ion-label>
+        <ion-select
+          interface="popover"
+          placeholder="Añade una valoración para esta ruta"
+        >
+          <ion-select-option value="1estrella">1 estrella</ion-select-option>
+          <ion-select-option value="2estrellas">2 estrellas</ion-select-option>
+          <ion-select-option value="3estrellas">3 estrellas</ion-select-option>
+          <ion-select-option value="4estrellas">4 estrellas</ion-select-option>
+          <ion-select-option value="5estrellas">5 estrellas</ion-select-option>
+        </ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Recomendaciones</ion-label>
+        <ion-select
+          interface="popover"
+          multiple="true"
+          placeholder="¿Quieres añadir alguna recomendación?"
+        >
+          <ion-select-option value="Gorra">Gorra</ion-select-option>
+          <ion-select-option value="RaquetasNieve"
+            >Raquetas de nieve</ion-select-option
+          >
+          <ion-select-option value="Agua">Agua</ion-select-option>
+          <ion-select-option value="Palos">Palos</ion-select-option>
+          <ion-select-option value="Brújula">Brújula</ion-select-option>
+          <ion-select-option value="Ropa">Ropa térmica</ion-select-option>
+        </ion-select>
+      </ion-item>
+    </ion-list>
+    <ion-button expand="block" fill="outline" color="dark">
+      Añadir ruta
+    </ion-button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+} from "@ionic/vue";
+
+import { resizeOutline, repeatOutline, flagOutline } from "ionicons/icons";
+
+export default defineComponent({
+  name: "AnadirRuta",
+  components: {
+  },
+  setup() {
+    return {
+      resizeOutline,
+      repeatOutline,
+      flagOutline,
+    };
+  },
+});
+</script>
--- trekkingCastillaYLeon/src/views/AnadirRuta.vue
+++ trekkingCastillaYLeon/src/views/AnadirRuta.vue
@@ -2,21 +2,57 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="">Atrás</ion-back-button>
+        </ion-buttons>
         <ion-title>Anadir ruta</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <h1>Añadir Ruta</h1>
+      <formulario-ruta></formulario-ruta>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+
+import FormularioRuta from '../components/FormularioRuta.vue'
+
+import { resizeOutline, repeatOutline, flagOutline } from "ionicons/icons";
 
 export default defineComponent({
-  name: 'AnadirRuta',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: "AnadirRuta",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonBackButton,
+    IonButtons,
+    FormularioRuta,
+  },
+  /*methods: {
+    saveMemory(memoryData){
+
+    }
+  },*/
+  setup() {
+    return {
+      resizeOutline,
+      repeatOutline,
+      flagOutline,
+    };
+  },
 });
 </script>
